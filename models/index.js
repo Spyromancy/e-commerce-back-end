35de534cbaeb4b10496993e6431fc205807e138a
Josh <josh.haddix598@gmail.com>
2022-08-18 16:38:33 -0500

    Set up model layouts

--- models/index.js
+++ models/index.js
@@ -6,16 +6,16 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "product_tag",
+  as: "product_tags",
   foreignKey: "product_id"
 })
 // Tags belongToMany Products (through ProductTag)
